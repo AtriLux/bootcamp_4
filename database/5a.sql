e266bdee5010998e910b92e66f5b908fec2b60da
AtriLux <90695137+AtriLux@users.noreply.github.com>
2022-08-01 12:45:43 +0700

    Delete 5a.sql

--- database/5a.sql
+++
@@ -1,5 +0,0 @@
-SELECT c.category_id, c.name, count(pc.product_id) as count
-	FROM category as c
-		LEFT JOIN product_category as pc ON c.category_id = pc.category_id
-				GROUP BY c.category_id, c.name
-					ORDER BY count DESC
