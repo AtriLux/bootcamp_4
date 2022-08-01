cf00a2e77129e83a6c042709205f5112be0eae5c
AtriLux <90695137+AtriLux@users.noreply.github.com>
2022-08-01 12:45:38 +0700

    Delete 5b.sql

--- database/5b.sql
+++
@@ -1,7 +0,0 @@
-SELECT * 
-	FROM (SELECT c.category_id, c.name, count(pc.product_id) as count
-			FROM category as c
-				JOIN product_category as pc ON c.category_id = pc.category_id
-					GROUP BY c.category_id, c.name) AS cc
-		WHERE cc.count >= 2
-			ORDER BY cc.count DESC
